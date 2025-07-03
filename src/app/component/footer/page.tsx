53c49e2cb86bacec062a29df557864134262b932
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-07-04 03:36:11 +0900

    add new varient

--- src/app/component/footer/page.tsx
+++ src/app/component/footer/page.tsx
@@ -2,18 +2,18 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="flex sm:flex-none">
-      <div className="footer sm:footer-horizontal bg-[#0E4242] text-base-content p-10 items-center justify-center">
+    <footer className="md:flex text-white">
+      <div className="footer sm:footer-horizontal bg-[#0E4242] p-10 md:p-0 items-center justify-center">
         <aside>
-          <div className="font-bold text-6xl pt-10">
+          <div className="font-bold text-4xl md:text-6xl pt-10">
             <span>
             Let’s chat
-            <i className="fa-solid fa-arrow-right ms-2.5"></i>
+            <i className="fa-solid fa-arrow-right ms-7"></i>
             </span>
           </div>
         </aside>
       </div>
-      <div className="footer bg-[#0B3131] text-base-content p-10 grid grid-cols-1 md:grid-cols-3">
+      <div className="footer bg-[#0B3131] p-10 grid grid-cols-1 md:grid-cols-2">
         <nav>
           <h6 className="footer-title">Animation & motion graphics</h6>
           <a className="link link-hover">IT consulting & advisory</a>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-white text-black dark:bg-black dark:text-white">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
