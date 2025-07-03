bf707965224b8661e83fdcdd921be20310716532
abdullah-al-mohammad <abdullahalmuhammad6@gmail.com>
2025-07-03 09:00:13 +0600

    add footer section

--- src/app/component/footer/page.tsx
+++ src/app/component/footer/page.tsx
@@ -2,14 +2,18 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div>
-      <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+    <footer className="flex sm:flex-none">
+      <div className="footer sm:footer-horizontal bg-[#0E4242] text-base-content p-10 items-center justify-center">
         <aside>
-          <h1 className="font-bold text-8xl">
+          <div className="font-bold text-6xl pt-10">
+            <span>
             Let’s chat
-            <span className="material-icons text-8xl">arrow_right_alt</span>
-          </h1>
+            <i className="fa-solid fa-arrow-right ms-2.5"></i>
+            </span>
+          </div>
         </aside>
+      </div>
+      <div className="footer bg-[#0B3131] text-base-content p-10 grid grid-cols-1 md:grid-cols-3">
         <nav>
           <h6 className="footer-title">Animation & motion graphics</h6>
           <a className="link link-hover">IT consulting & advisory</a>
@@ -24,15 +28,24 @@ export const Footer = () => {
           <a className="link link-hover">Website development</a>
           <a className="link link-hover">UI-UX design</a>
         </nav>
-      </footer>
-      <footer className="footer sm:footer-horizontal footer-center text-base-content p-4 bg-base-200">
-        <aside>
-          <p>
-            Copyright © {new Date().getFullYear()} - All right reserved by ACME
-            Industries Ltd
-          </p>
-        </aside>
-      </footer>
-    </div>
+        <nav>
+        <h6 className="footer-title">Contact Info</h6>
+            <h3>
+              © {new Date().getFullYear()} NextEra
+            </h3>
+            <p>
+              Email: contact@nextera.com
+            </p>
+            <p>
+              Address: Dhaka, Bangladesh
+            </p>
+            <p className="flex gap-2">
+              <a href="">About</a>
+              <a href="">Privacy Policy</a>
+              <a href="">Terms of Service</a>
+            </p>
+          </nav>
+      </div>
+    </footer>
   );
 };
